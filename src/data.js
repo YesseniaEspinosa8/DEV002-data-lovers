91519e5720ee42aadfc68a5112bebfae83bf79fa
YesseniaEspinosa8 <yeyeespinosa8gmail.com>
2023-01-12 23:52:18 -0600

    Edite HTM y CSS

--- src/data.js
+++ src/data.js
@@ -1,4 +1,11 @@
 // estas funciones son de ejemplo
+export default class API {
+  getCharacter(id) {
+    fetch("https://rickandmortyapi.com/api/character/2/${id}")
+    .then ((response) =>response.json())
+    .then((data) => console.log(data))
+  }
+}
 
 export const example = () => {
   return 'example';
