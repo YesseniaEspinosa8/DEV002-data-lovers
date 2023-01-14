d23db4222283550124bf35eacb72d262966cf747
YesseniaEspinosa8 <yeyeespinosa8gmail.com>
2023-01-14 17:13:19 -0600

    HTML Dinamico

--- src/data.js
+++ src/data.js
@@ -7,6 +7,7 @@ export default class API {
   }
 }
 
+
 export const example = () => {
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 
 console.log(example, data.results);
+
