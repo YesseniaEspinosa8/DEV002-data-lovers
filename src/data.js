e70e6e8ac3b8309ac61429a7afb34e96b10783fd
YesseniaEspinosa8 <yeyeespinosa8gmail.com>
2023-01-16 19:41:46 -0600

    Mostrar data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,31 @@
-// estas funciones son de ejemplo
-export default class API {
-  getCharacter(id) {
-    fetch("https://rickandmortyapi.com/api/character/2/${id}")
-    .then ((response) =>response.json())
-    .then((data) => console.log(data))
-  }
+export const extrayendoData = (datos) => {
+  return (Object.values(datos.data));
 }
 
+export const filtrandoInformacion = (selectYear, data) => {
+  const filtroYear = data.filter(year => year.tags.includes(selectYear));
+  return 'filtroYear';
+}
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
+export const ordenandoPersonajes = (sortOrder, data) => {
+  const orDatos = [...data].sort((a,b) =>{
+    if (a.name > b.name) {
+      if (sortOrder === "az"){
+        return 1;
+      }
+      else {
+        return -1;
+      }
+    }
+    if(a.name < b.name){
+      if(sortOrder === "za"){
+        return -1;
+      }
+      else {
+        return 1;
+      }
+    }
+    return 0;
+  });
+  return orDatos;
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import { example } from './data.js';
-
 import data from './data/rickandmorty/rickandmorty.js';
+console.log(data.results)
+const personajes =data.results
+const contenedor = document.getElementById("contenedor");
+console.log(contenedor)
+
+personajes.forEach(personaje =>{
+    console.log(personaje.name)
+    console.log(personaje.image)
+    contenedor.innerHTML+= `
+    <div class="card">
+    <img class="imagen" src="${personaje.image}"  alt="imagen del personaje"/>
+    <p>${personaje.name}</p>
+    </div>
+    
+    `
+
+})
 
 
-console.log(example, data.results);
+
+
+
 
