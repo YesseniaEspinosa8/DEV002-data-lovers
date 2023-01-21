bd2d07e874cc35b6aceaf5437501654c9d74a227
YesseniaEspinosa8 <yeyeespinosa8gmail.com>
2023-01-20 19:56:39 -0600

    Centrar tarjetas

--- src/data.js
+++ src/data.js
@@ -1,16 +1,8 @@
-// CALCULO***
-//export const promByScore = (data) => {
-  //const total = data.map(n => Number(m.rt_score)).reduce(function (a.b) { return a + b; }, 0);
-  //const promedio = total / dato.length
-  //return promedio;
-//}
+
 //Personajes
 export const filterbyCharacters = (string, data) => {
   const results = data.filter(p => p.name [0] === string);
   return results;
 }
 
-//Temporadas 
-export const sortByEpisodes = (data) => {
-  
-}
+
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ console.log(contenedor)
 
 
 const displayCard =(dataRam) => {
-    contenedor.innerHTML=""
+    contenedor.innerHTML= ""
     dataRam.forEach(personaje => {
         console.log(personaje.name)
         console.log(personaje.image)
@@ -16,7 +16,7 @@ const displayCard =(dataRam) => {
         <img class="imagen" src="${personaje.image}"  alt="imagen del personaje"/>
         <p>${personaje.name}</p>
         </div>  
-        `
+        `    
     })
 
 } 
@@ -27,256 +27,256 @@ const btnA= document.getElementById("letraA");
 
 btnA.addEventListener("click", () => {
     const resultsA=filterbyCharacters("A",personajes)
-    console.log(resultsA)
+    //console.log(resultsA)
     displayCard(resultsA)
 })
 
-displayCard(personajes)
+
 
 const btnB= document.getElementById("letraB");
 
 btnB.addEventListener("click", () => {
     const resultsB=filterbyCharacters("B",personajes)
-    console.log(resultsB)
+   // console.log(resultsB)
     displayCard(resultsB)
 })
 
-displayCard(personajes)
+
 
 const btnC= document.getElementById("letraC");
 
 btnC.addEventListener("click", () => {
     const resultsC=filterbyCharacters("C",personajes)
-    console.log(resultsC)
+    //console.log(resultsC)
     displayCard(resultsC)
 })
 
-displayCard(personajes)
+
 
 const btnD= document.getElementById("letraD");
 
 btnD.addEventListener("click", () => {
-    const resultsB=filterbyCharacters("D",personajes)
-    console.log(resultsD)
+    const resultsD=filterbyCharacters("D",personajes)
+    //console.log(resultsD)
     displayCard(resultsD)
 })
 
-displayCard(personajes)
+
 
 const btnE= document.getElementById("letraE");
 
 btnE.addEventListener("click", () => {
     const resultsE=filterbyCharacters("E",personajes)
-    console.log(resultsE)
+    //console.log(resultsE)
     displayCard(resultsE)
 })
 
-displayCard(personajes)
+
 
 const btnF= document.getElementById("letraF");
 
 btnF.addEventListener("click", () => {
     const resultsF=filterbyCharacters("F",personajes)
-    console.log(resultsF)
+    //console.log(resultsF)
     displayCard(resultsF)
 })
 
-displayCard(personajes)
+
 
 const btnG= document.getElementById("letraG");
 
 btnG.addEventListener("click", () => {
     const resultsG=filterbyCharacters("G",personajes)
-    console.log(resultsG)
+    //console.log(resultsG)
     displayCard(resultsG)
 })
 
-displayCard(personajes)
+
 
 const btnH= document.getElementById("letraH");
 
 btnH.addEventListener("click", () => {
     const resultsH=filterbyCharacters("H",personajes)
-    console.log(resultsH)
+    //console.log(resultsH)
     displayCard(resultsH)
 })
 
-displayCard(personajes)
+
 
 const btnI= document.getElementById("letraI");
 
 btnI.addEventListener("click", () => {
     const resultsI=filterbyCharacters("I",personajes)
-    console.log(resultsI)
+    //console.log(resultsI)
     displayCard(resultsI)
 })
 
-displayCard(personajes)
+
 
 const btnJ= document.getElementById("letraJ");
 
 btnJ.addEventListener("click", () => {
     const resultsJ=filterbyCharacters("J",personajes)
-    console.log(resultsJ)
+    //console.log(resultsJ)
     displayCard(resultsJ)
 })
 
-displayCard(personajes)
+
 
 const btnK= document.getElementById("letraK");
 
 btnK.addEventListener("click", () => {
     const resultsK=filterbyCharacters("K",personajes)
-    console.log(resultsK)
+    //console.log(resultsK)
     displayCard(resultsK)
 })
 
-displayCard(personajes)
+
 
 const btnL= document.getElementById("letraL");
 
 btnL.addEventListener("click", () => {
-    const resultsL=filterbyCharacters("letraL",personajes)
-    console.log(resultsL)
+    const resultsL=filterbyCharacters("L",personajes)
+    //console.log(resultsL)
     displayCard(resultsL)
 })
 
-displayCard(personajes)
+
 
 const btnM= document.getElementById("letraM");
 
 btnM.addEventListener("click", () => {
     const resultsM=filterbyCharacters("M",personajes)
-    console.log(resultsM)
+    //console.log(resultsM)
     displayCard(resultsM)
 })
 
-displayCard(personajes)
+
 
 const btnN= document.getElementById("letraN");
 
 btnN.addEventListener("click", () => {
-    const resultsB=filterbyCharacters("N",personajes)
-    console.log(resultsN)
+    const resultsN=filterbyCharacters("N",personajes)
+    //console.log(resultsN)
     displayCard(resultsN)
 })
 
-displayCard(personajes)
+
 
 const btnO= document.getElementById("letraO");
 
 btnO.addEventListener("click", () => {
     const resultsO=filterbyCharacters("O",personajes)
-    console.log(resultsO)
+    //console.log(resultsO)
     displayCard(resultsO)
 })
 
-displayCard(personajes)
+
 
 const btnP= document.getElementById("letraP");
 
 btnP.addEventListener("click", () => {
     const resultsP=filterbyCharacters("P",personajes)
-    console.log(resultsP)
+    //console.log(resultsP)
     displayCard(resultsP)
 })
 
-displayCard(personajes)
+
 
 const btnQ= document.getElementById("letraQ");
 
 btnQ.addEventListener("click", () => {
     const resultsQ=filterbyCharacters("Q",personajes)
-    console.log(resultsQ)
+    //console.log(resultsQ)
     displayCard(resultsQ)
 })
 
-displayCard(personajes)
+
 
 const btnR= document.getElementById("letraR");
 
 btnR.addEventListener("click", () => {
     const resultsR=filterbyCharacters("R",personajes)
-    console.log(resultsR)
+    //console.log(resultsR)
     displayCard(resultsR)
 })
 
-displayCard(personajes)
+
 
 const btnS= document.getElementById("letraS");
 
 btnS.addEventListener("click", () => {
     const resultsS=filterbyCharacters("S",personajes)
-    console.log(resultsS)
+    //console.log(resultsS)
     displayCard(resultsS)
 })
 
-displayCard(personajes)
+
 
 const btnT= document.getElementById("letraT");
 
 btnT.addEventListener("click", () => {
     const resultsT=filterbyCharacters("T",personajes)
-    console.log(resultsT)
+    //console.log(resultsT)
     displayCard(resultsT)
 })
 
-displayCard(personajes)
+
 
 const btnU= document.getElementById("letraU");
 
 btnU.addEventListener("click", () => {
     const resultsU=filterbyCharacters("U",personajes)
-    console.log(resultsU)
+    //console.log(resultsU)
     displayCard(resultsU)
 })
 
-displayCard(personajes)
+
 
 const btnV= document.getElementById("letraV");
 
 btnV.addEventListener("click", () => {
     const resultsV=filterbyCharacters("V",personajes)
-    console.log(resultsV)
+    //console.log(resultsV)
     displayCard(resultsV)
 })
 
-displayCard(personajes)
+
 
 const btnW= document.getElementById("letraW");
 
 btnW.addEventListener("click", () => {
     const resultsW=filterbyCharacters("W",personajes)
-    console.log(resultsW)
+    //console.log(resultsW)
     displayCard(resultsW)
 })
 
-displayCard(personajes)
+
 
 const btnX= document.getElementById("letraX");
 
 btnX.addEventListener("click", () => {
     const resultsX=filterbyCharacters("X",personajes)
-    console.log(resultsX)
+    //console.log(resultsX)
     displayCard(resultsX)
 })
 
-displayCard(personajes)
+
 
 const btnY= document.getElementById("letraY");
 
 btnY.addEventListener("click", () => {
     const resultsY=filterbyCharacters("Y",personajes)
-    console.log(resultsY)
+    //console.log(resultsY)
     displayCard(resultsY)
 })
 
-displayCard(personajes)
+
 
 const btnZ= document.getElementById("letraZ");
 
 btnZ.addEventListener("click", () => {
     const resultsZ=filterbyCharacters("Z",personajes)
-    console.log(resultsZ)
+    //console.log(resultsZ)
     displayCard(resultsZ)
 })
